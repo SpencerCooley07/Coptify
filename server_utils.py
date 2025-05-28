0ca6ef12a801cc11377c4717a03806c070b04aea
SpencerCooley07 <spencerjcooley07@gmail.com>
2025-05-28 15:38:51 +1000

    Added bulk song and playlist upload

--- server_utils.py
+++ server_utils.py
@@ -15,9 +15,9 @@ cursor = connection.cursor()
 runProgram = True
 
 while runProgram == True:
-    clear()
-    match input('SERVER UTILS\n1. Upload song\n2. Bulk upload songs\n3. Upload playlist\n> '):
+    match input('SERVER UTILS\n1. Upload song\n2. Bulk upload songs\n3. Bulk upload to playlist\n> '):
         case '1':
+            clear()
             path = input('File Location: ')
             filename = path.split('/')[-1]
             if os.path.exists(path):
@@ -27,6 +27,57 @@ while runProgram == True:
                 cursor.execute("INSERT INTO songs (songID, name, artist) VALUES (?, ?, ?)", (songID, name, artist))
                 connection.commit(), print('Copied '+f'src/songs/{filename}')
                 runProgram = False if input('Exit? (Y/N) ') == "y" else True
+            else: runProgram = False if input('Invalid file path, exit? (Y/N) ') == "y" else True
 
+        case '2':
+            clear()
+            print('The input directory must contain a\n• list.csv\n• All Songs')
+            print('The list file must contian songs separated by rows, and each row contains "name, artist, filename"\n')
+            path = input('Input Directory: ')
+            if os.path.exists(path) and os.path.exists(path + '/list.csv'):
+                with open(path + '/list.csv', 'r') as inputList:
+                    for line in inputList:
+                        line = line.strip('\n').split(',')
+
+                        songID = str(uuid.uuid4())
+                        shutil.copy(path + f'/{line[2]}', CURRENT_DIR + f'/src/assets/songs/{songID}.mp3')
+                        cursor.execute("INSERT INTO songs (songID, name, artist) VALUES (?, ?, ?)", (songID, line[0], line[1]))
+                        print(f'{songID}, {line[0]}, {line[1]}')
+
+                    connection.commit()
+                print('Songs Uploaded!')
+                runProgram = False if input('Exit? (Y/N) ') == "y" else True
+            else: runProgram = False if input('Invalid file path, exit? (Y/N) ') == "y" else True
+
+        case '3':
+            clear()
+            print('The input directory must contain a\n• list.csv\n• All Songs')
+            print('The list file must contian songs separated by rows, and each row contains "name, artist, filename"\n')
+            path = input('Input Directory: ')
+            playlistName = input('Playlist Name: ')
+
+            if os.path.exists(path) and os.path.exists(path + '/list.csv'):
+                playlistID = str(uuid.uuid4())
+
+                cursor.execute("INSERT INTO playlists (playlistID, curator, name, isPublic) VALUES (?, ?, ?, ?)", (playlistID, 'Coptify', playlistName, 1))
+                connection.commit()
+
+                with open(path + '/list.csv', 'r') as inputList:
+                    for i, line in enumerate(inputList):
+                        line = line.strip('\n').split(',')
+
+                        songID = str(uuid.uuid4())
+
+                        shutil.copy(path + f'/{line[2]}', CURRENT_DIR + f'/src/assets/songs/{songID}.mp3')
+                        cursor.execute("INSERT INTO songs (songID, name, artist) VALUES (?, ?, ?)", (songID, line[0], line[1]))
+                        cursor.execute("INSERT INTO playlistSongs (playlistID, songID, position) VALUES (?, ?, ?)", (playlistID, songID, i+1))
+                        print(f'{songID}, {line[0]}, {line[1]}')
+
+                    connection.commit()
+                print(f'Playlist ({playlistName}) Uploaded!')
+                runProgram = False if input('Exit? (Y/N) ') == "y" else True
+            else: runProgram = False if input('Invalid file path, exit? (Y/N) ') == "y" else True
+
+        case 'exit': runProgram = False
         case _:
             runProgram = False if input('Invalid, exit? (Y/N) ') == "y" else True
