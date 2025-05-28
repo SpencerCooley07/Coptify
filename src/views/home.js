8f9e86f06ad87f8cd24f155adcfae0b75c5cecf1
SpencerCooley07 <spencerjcooley07@gmail.com>
2025-05-28 21:35:54 +1000

    Added 'Coptify Playlists' section with api call

--- src/views/home.js
+++ src/views/home.js
@@ -1,6 +1,6 @@
 export function renderHome() {
-    const content = document.getElementById('content');
-    content.innerHTML = `
+    const page = document.getElementById('page');
+    page.innerHTML = `
         <nav class="navbar">
             <div class="nav-left">
                 <div class="logo">
@@ -17,8 +17,45 @@ export function renderHome() {
             </div>
 
             <div class="nav-right">
-                <img id="profile-icon" src="/src/assets/profile.png" alt="Profile" class="profile-icon">
+                <a href="/login"><img id="profile-icon" src="/src/assets/profile.png" alt="Profile" class="profile-icon"></a>
             </div>
         </nav>
+        <div class="content">
+            <div class="coptify-playlists-container">
+                <h1>Coptify Playlists</h1>
+                <div id="coptify-playlists" class="coptify-playlists">
+                </div>
+            </div>
+        </div>
     `;
+    getCoptifyPlaylists();
+};
+
+async function getCoptifyPlaylists() {
+    const coptifyPlaylists = document.getElementById('coptify-playlists')
+
+    try {
+        const response = await fetch('/api/getCoptifyPlaylists', {
+            method: 'GET'
+        });
+        const responseJSON = await response.json();
+
+        if (!response.ok) {
+            alert(responseJSON.message);
+        } else {
+            console.log(responseJSON);
+            for (var key in responseJSON) {
+                coptifyPlaylists.insertAdjacentHTML('beforeend', `
+                <div id="${key}" class="content-item">
+                    <img src='/src/assets/playlists/${key}.jpg'>
+                    <h2>${responseJSON[key]['name']}</h2>
+                    <h3>${responseJSON[key]['curator']}</h3>
+                </div>
+                `);
+            };
+        };
+    } catch (error) {
+        alert('Could not retrieve data');
+        console.log(error);
+    };
 };
