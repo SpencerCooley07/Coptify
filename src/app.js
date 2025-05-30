66a4192026f4c7eb0692c0e85d80036becad001a
SpencerCooley07 <spencerjcooley07@gmail.com>
2025-05-30 15:10:08 +1000

    Added initPlayer to app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
+import { initPlayer } from "./player.js";
 import { initRouter } from "./router.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     initRouter();
+    initPlayer();
 });
